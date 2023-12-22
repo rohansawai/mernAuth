e8099cc1b9644468f9596fecc5b12c749cb36750
Rohan Sawai <sawairohan90@gmail.com>
2023-12-22 15:08:07 +0530

    user controller/error handler

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,12 +1,38 @@
-import asyncHandler from 'express-async-handler';
+import asyncHandler from "express-async-handler";
 
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
 const authUser = asyncHandler(async (req, res) => {
-    res.status(200).json({message: 'Auth User'});
+  res.status(200).json({ message: "Auth User" });
 });
 
-export {
-    authUser
-};
+// @desc Register new user
+// route POST /api/users
+// @access Public
+const registerUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Register User" });
+});
+
+// @desc Logout User user
+// route POST /api/users/logout
+// @access Public
+const logoutUser = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "Logout User" });
+});
+
+// @desc Get user profile
+// route GET /api/users/profile
+// @access Private
+const getUserProfile = asyncHandler(async (req, res) => {
+  res.status(200).json({ message: "User Profile" });
+});
+
+// @desc Update user profile
+// route PUT /api/users/profile
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+    res.status(200).json({ message: "Update User Profile" });
+  });
+
+export { authUser };
