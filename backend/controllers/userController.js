63227aadb6d156c716070fa204d121dd797740ec
Rohan Sawai <sawairohan90@gmail.com>
2023-12-24 13:48:01 +0530

    jwt token added

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,6 @@
 import asyncHandler from "express-async-handler";
-
+import User from "../models/userModel.js";
+import generateToken from "../../utils/generateToken.js";
 // @desc Auth user/set token
 // route POST /api/users/auth
 // @access Public
@@ -11,7 +12,30 @@ const authUser = asyncHandler(async (req, res) => {
 // route POST /api/users
 // @access Public
 const registerUser = asyncHandler(async (req, res) => {
-  res.status(200).json({ message: "Register User" });
+  const { name, email, password } = req.body;
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error("User already exists");
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error("Invalid user data");
+  }
 });
 
 // @desc Logout User user
@@ -32,7 +56,13 @@ const getUserProfile = asyncHandler(async (req, res) => {
 // route PUT /api/users/profile
 // @access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-    res.status(200).json({ message: "Update User Profile" });
-  });
+  res.status(200).json({ message: "Update User Profile" });
+});
 
-export { authUser, registerUser, logoutUser, getUserProfile, updateUserProfile }; 
+export {
+  authUser,
+  registerUser,
+  logoutUser,
+  getUserProfile,
+  updateUserProfile,
+};
