ab1d727d6dbbe6c34d0c669ca9f0af4edb9f3b02
Rohan Sawai <sawairohan90@gmail.com>
2023-12-27 12:19:31 +0530

    removed href and added linkContainer instead

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import { Outlet } from "react-router-dom";
-
+import { Container } from "react-bootstrap";
 const App = () => {
   return (
     <div>
       <Header />
-      <Outlet />{" "}
+      <Container className="my-2">
+        <Outlet />
+      </Container>
     </div>
   );
 };
