382efdfe94e57bd3270edc1135cc11d10525d592
Rohan Sawai <sawairohan90@gmail.com>
2023-12-27 19:19:35 +0530

    added api slice

--- frontend/src/slices/apiSlice.ts
+++ frontend/src/slices/apiSlice.ts
@@ -1,1 +1,9 @@
-import { CreateApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+import {  createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const baseQuery = fetchBaseQuery({baseUrl: ''});
+
+export const apiSlice = createApi({
+    baseQuery,
+    tagTypes: ['User'],
+    endpoints: (builder) => ({})
+})
--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -1,10 +1,12 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from "./slices/authSlice"
+import authReducer from "./slices/authSlice";
+import { apiSlice } from "./slices/apiSlice";
 const store = configureStore({
   reducer: {
-    auth: authReducer
+    auth: authReducer,
+    [apiSlice.reducerPath]: apiSlice.reducer,
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware(),
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(apiSlice.middleware),
   devTools: true,
 });
 
